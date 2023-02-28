b26650bbfb301b0d4f3b674e6d51ba8f48bdd48f
Deepak Nadig <nadig@purdue.edu>
2023-02-27 21:37:41 -0500

    Bump Ubuntu to 22.04 and K8s to 1.24.9

--- kubernetes/bootstrap.sh
+++ kubernetes/bootstrap.sh
@@ -1,96 +1,62 @@
 #!/bin/bash
 
-# Update hosts file
-echo "[TASK 1] Update /etc/hosts file"
-cat >>/etc/hosts<<EOF
-172.42.42.100 master.example.com master
-172.42.42.101 worker1.example.com worker1
-172.42.42.102 worker2.example.com worker2
-EOF
-
-
-echo "[TASK 2] Install docker container engine"
-apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update -y
-apt-get install docker-ce -y
-
-# add ccount to the docker group
-usermod -aG docker vagrant
-
-# Following configurations are recomended in the kubenetes documentation for Docker runtime.
-# Please refer to https://kubernetes.io/docs/setup/production-environment/container-runtimes/#docker
-
-cat <<EOF | sudo tee /etc/docker/daemon.json
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-
-echo "Docker Runtime Configured Successfully"
-
-# Enable docker service
-echo "[TASK 3] Enable and start docker service"
-systemctl enable docker >/dev/null 2>&1
-systemctl daemon-reload
-sudo systemctl restart docker
-
-
-# Add sysctl settings
-echo "[TASK 6] Add sysctl settings"
-cat >>/etc/sysctl.d/kubernetes.conf<<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sysctl --system >/dev/null 2>&1
-
-# Disable swap
-echo "[TASK 7] Disable and turn off SWAP"
+echo "[TASK 1] Disable and turn off SWAP"
 sed -i '/swap/d' /etc/fstab
 swapoff -a
 
-# Install apt-transport-https pkg
-echo "[TASK 8] Installing apt-transport-https pkg"
-apt-get update && apt-get install -y apt-transport-https curl ca-certificates 
-# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "[TASK 2] Stop and Disable firewall"
+systemctl disable --now ufw >/dev/null 2>&1
 
-# Add he kubernetes sources list into the sources.list directory
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-# deb https://apt.kubernetes.io/ kubernetes-xenial main
-deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main
+echo "[TASK 3] Enable and Load Kernel modules"
+cat >>/etc/modules-load.d/containerd.conf<<EOF
+overlay
+br_netfilter
 EOF
-# verify
-ls -ltr /etc/apt/sources.list.d/kubernetes.list
+modprobe overlay
+modprobe br_netfilter
 
-apt-get update -y
+echo "[TASK 4] Add Kernel settings"
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+EOF
+sysctl --system >/dev/null 2>&1
 
-# Install Kubernetes
-echo "[TASK 9] Install Kubernetes kubeadm, kubelet and kubectl"
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+echo "[TASK 5] Install containerd runtime"
+apt update -qq >/dev/null 2>&1
+apt install -qq -y ca-certificates curl gnupg lsb-release >/dev/null 2>&1
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg >/dev/null 2>&1
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update -qq >/dev/null 2>&1
+apt install -qq -y containerd.io >/dev/null 2>&1
+containerd config default > /etc/containerd/config.toml
+sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+systemctl restart containerd
+systemctl enable containerd >/dev/null 2>&1
 
-# Start and Enable kubelet service
-echo "[TASK 10] Enable and start kubelet service"
-systemctl enable kubelet >/dev/null 2>&1
-systemctl start kubelet >/dev/null 2>&1
+echo "[TASK 6] Add apt repo for kubernetes"
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >/dev/null 2>&1
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/null 2>&1
 
-# Enable ssh password authentication
-echo "[TASK 11] Enable ssh password authentication"
+echo "[TASK 7] Install Kubernetes components (kubeadm, kubelet and kubectl)"
+apt install -qq -y kubeadm=1.24.9-00 kubelet=1.24.9-00 kubectl=1.24.9-00 >/dev/null 2>&1
+
+echo "[TASK 8] Enable ssh password authentication"
 sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 systemctl reload sshd
 
-# Set Root password
-echo "[TASK 12] Set root password"
-echo -e "kubeadmin\nkubeadmin" | passwd root
-#echo "kubeadmin" | passwd --stdin root >/dev/null 2>&1
+echo "[TASK 9] Set root password"
+echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+echo "export TERM=xterm" >> /etc/bash.bashrc
 
-# Update vagrant user's bashrc file
-echo "export TERM=xterm" >> /etc/bashrc
+echo "[TASK 10] Update /etc/hosts file"
+cat >>/etc/hosts<<EOF
+172.42.42.100   master.example.com     master
+172.42.42.101   worker1.example.com    worker1
+172.42.42.102   worker2.example.com    worker2
+EOF
--- kubernetes/bootstrap_master.sh
+++ kubernetes/bootstrap_master.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-# Initialize Kubernetes
-echo "[TASK 1] Initialize Kubernetes Cluster"
+#!/bin/bash
+
+echo "[TASK 1] Pull required containers"
+kubeadm config images pull >/dev/null 2>&1
+
+echo "[TASK 2] Initialize Kubernetes Cluster"
 kubeadm init --apiserver-advertise-address=172.42.42.100 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
 
-# Copy Kube admin config
-echo "[TASK 2] Copy kube admin config to Vagrant user .kube directory"
-mkdir /home/vagrant/.kube
-cp /etc/kubernetes/admin.conf /home/vagrant/.kube/config
-chown -R vagrant:vagrant /home/vagrant/.kube
-
-# Deploy flannel network
 echo "[TASK 3] Deploy Calico network"
-su - vagrant -c "kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml"
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/tigera-operator.yaml >/dev/null 2>&1
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/custom-resources.yaml >/dev/null 2>&1
 
-# Generate Cluster join command
-echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
-kubeadm token create --print-join-command > /joincluster.sh
+# Copy kube admin configuration to user $HOME
+echo "[TASK 4] Copy kube admin config to Vagrant user .kube directory"
+mkdir /home/vagrant/.kube
+sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+sudo chown -R vagrant:vagrant /home/vagrant/.kube
+# Or alternatively
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+
+echo "[TASK 5] Generate and save cluster join command to /joincluster.sh"
+kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
